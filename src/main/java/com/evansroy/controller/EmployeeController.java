4b8810426b843d27f09ea6c883b7e522796dac0a
evansroy <evansroysir@gmail.com>
2023-03-09 12:35:41 +0300

    Add EmployeeController.java and create createEmployees post method

--- src/main/java/com/evansroy/controller/EmployeeController.java
+++ src/main/java/com/evansroy/controller/EmployeeController.java
@@ -3,9 +3,7 @@ package com.evansroy.controller;
 import com.evansroy.model.Employee;
 import com.evansroy.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,11 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getEmployees(){
         return employeeRepository.findAll();
+    }
+
+    //build create employee REST API
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 }
