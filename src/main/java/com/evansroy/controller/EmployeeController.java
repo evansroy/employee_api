e1717eda0b87a40f4e9cc8c07a334c84f2eb96b9
evansroy <evansroysir@gmail.com>
2023-03-09 12:56:06 +0300

    Add build get employee by id REST API

--- src/main/java/com/evansroy/controller/EmployeeController.java
+++ src/main/java/com/evansroy/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.evansroy.controller;
 
+import com.evansroy.exception.ResourceNotFoundException;
 import com.evansroy.model.Employee;
 import com.evansroy.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,5 +24,13 @@ public class EmployeeController {
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    //build get employee by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+        return ResponseEntity.ok(employee);
     }
 }
